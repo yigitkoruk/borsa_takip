c38c180e75aef5327b0bce9132e5c458f16799e6
Ömer Kılıç <komer4542@gmail.com>
2023-11-10 16:24:58 +0300

    header ve footer düzenlendi

---
+++ Borsa Takip/views/partials/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Borsa Takip Sayfası</title>
+    <link rel="shortcut icon" href="image/dolar.png" type="image/x-icon" text-align:5 px>
+    <link rel="stylesheet" href="style/style.css">
+</head>
