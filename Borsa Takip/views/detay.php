d10091c24070b67aabf76f5618251065c14e1eb3
Ömer Kılıç <komer4542@gmail.com>
2023-11-12 21:07:26 +0300

    detay safasındaki hatalar çözüldü

--- Borsa Takip/views/detay.php
+++ Borsa Takip/views/detay.php
@@ -8,19 +8,19 @@ include "partials/header.php";
     <h1 style="text-align: center;">Hisse Güncelleme</h1>
         <form id="borsaForm" method="POST">
             <label for="hisseAdi">Hisse Adı:</label>
-            <input type="text" id="hisseAdi" name="hisseAdi" value="<?= $row["hisse_adi"] ? $row["hisse_adi"] : "Hata" ?>">
+            <input type="text" id="hisseAdi" name="hisseAdi" value="<?= $hisseDetay[0]["hisse_adi"] ? $hisseDetay[0]["hisse_adi"] : "Hata" ?>">
             <?= $hisseAdi_Hata ?? "" ?><br>
 
             <label for="alisMaliyeti">Alış Maliyeti:</label>
-            <input type="number" id="alisMaliyeti" name="alisMaliyeti" value="<?= $row["alis_maliyeti"] ? $row["alis_maliyeti"] : "Hata" ?>">
+            <input type="number" id="alisMaliyeti" name="alisMaliyeti" value="<?= $hisseDetay[0]["alis_maliyeti"] ? $hisseDetay[0]["alis_maliyeti"] : "Hata" ?>">
             <?= $alisMaliyeti_Hata ?? "" ?><br>
 
             <label for="satisFiyati">Güncel Fiyat:</label>
-            <input type="number" id="satisFiyati" name="satisFiyati" value="<?= $row["guncel_fiyat"] ? $row["guncel_fiyat"] : "Hata" ?>">
+            <input type="number" id="satisFiyati" name="satisFiyati" value="<?= $hisseDetay[0]["guncel_fiyat"] ? $hisseDetay[0]["guncel_fiyat"] : "Hata" ?>">
             <?= $satisFiyati_Hata ?? "" ?><br>
 
             <label for="adet">Adet:</label>
-            <input type="number" id="adet" name="adet" value="<?= $row["adet"] ? $row["adet"] : "Hata" ?>">
+            <input type="number" id="adet" name="adet" value="<?= $hisseDetay[0]["adet"] ? $hisseDetay[0]["adet"] : "Hata" ?>">
             <?= $Adet_Hata ?? "" ?><br>
 
             <input type="submit" name="guncelle" value="Güncelle">
