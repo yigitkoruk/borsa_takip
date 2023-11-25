ff4e9f1c4476c5dd9c6efec52f45f3e214d86052
Ömer Kılıç <komer4542@gmail.com>
2023-11-25 13:21:24 +0300

    index Controller güncellendi

--- Borsa Takip/controller/indexController.php
+++ Borsa Takip/controller/indexController.php
@@ -1,19 +1,9 @@
 <?php
-include "../models/model.php";
+include "backRound.php";
+include_once "../models/model.php";
 $model = new MODEL();
-date_default_timezone_set('Europe/Istanbul');
-$saat = date('H');
-$gün = date('d');
-$ay = date("F");
 $list = $model->hisseList();
 $row = $model->gunlukHisse2();
-
-$toplamKarZarar = 0;
-foreach ($list as $item) {
-    $toplamKarZarar += $item["kar_zarar"];
-}
-
-$activeTab = isset($_GET['tab']) ? $_GET['tab'] : 'alisSatis';
 
 if (isset($_POST["ekle"])) {
     $hisseAdi = $_POST["hisseAdi"];
@@ -68,39 +58,4 @@ if (isset($_POST["detay"])) {
     session_start();
     $_SESSION["id"] = $_POST["hidden"];
     header("Location: detay.php");
-}
-
-if ($saat == '18') {
-    foreach ($list as $item) {
-        $hisseBilgisi = [
-            "value1" => $item["id"],
-            "value2" => $item["alis_maliyeti"],
-            "value3" => $item["guncel_fiyat"],
-            "value4" => $item["adet"],
-            "value5" => $item["kar_zarar"],
-            "value6" => $ay,
-        ];
-        $model->gunlukHisse($hisseBilgisi);
-    }
-
-    $hisseBilgisi = [
-        "value1" => $toplamKarZarar,
-        "value2" => $gün,
-        "value3" => $ay,
-    ];
-    $model->toplamGunlukKarZarar($hisseBilgisi);
-}
-
-if ($gün == 1) {
-    $aylıkHesapama = $model->aylıkHesaplama($ay);
-    
-    foreach ($aylıkHesapama as $item) {
-        $aylıkKarZarar += $item["kar_zarar"];
-    }
-
-    $hisseBilgisi = [
-        "value1" => $ay,
-        "value2" => $aylıkKarZarar,
-    ];
-    $model->aylıkKarZarar($hisseBilgisi);
 }
